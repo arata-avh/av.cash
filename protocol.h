bb22ad8f703074a04523fdb123a564fa6aafda92
arata <arata@av.cash>
2018-07-19 20:42:07 +0800

    add nettype avhtest

--- protocol.h
+++ protocol.h
@@ -237,6 +237,10 @@ extern const char *GETBLOCKTXN;
  * @since protocol version 70014 as described by BIP 152
  */
 extern const char *BLOCKTXN;
+/*
+ * AVH test
+*/
+extern const char *AVHTEST;
 };
 
 /* Get a vector of all valid message types (see above) */
